ef553a156713cee2965dfe1091ac9d38ebac4715
SeungkyunNA <n.jake270705@gmail.com>
2022-11-15 19:43:53 +0100

    Create InvertTree.java

---
+++ week2/InvertTree.java
@@ -0,0 +1,46 @@
+package week2;
+
+public class InvertTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        
+        if(root == null){
+            return root;
+        }
+        if(root.left == null && root.right == null){
+            return root;
+        }
+        
+        
+        TreeNode leftChild;
+        TreeNode rightChild;
+        
+        if(root.left != null){
+             leftChild = root.left;
+        }
+        else {
+            leftChild = null;
+        }
+        
+        if(root.right != null){
+            rightChild = root.right;
+        }
+        else{
+            rightChild = null;
+        }
+        
+        root.left = rightChild;
+        root.right = leftChild;
+        
+        if (root.left != null){
+            invertTree(root.left);
+        }
+        if (root.right != null){
+            invertTree(root.right);
+        }
+        
+        return root;
+        
+    }
+    
+}
