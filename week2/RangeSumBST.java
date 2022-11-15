196a6b7ea32e7e8a9c7e9f9475c4e2d60907ce3b
SeungkyunNA <n.jake270705@gmail.com>
2022-11-15 21:12:39 +0100

    Create RangeSumBST.java

---
+++ week2/RangeSumBST.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/range-sum-of-bst/
+
+package week2;
+
+public class RangeSumBST {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        
+        if(root != null){
+            
+            if(root.val >= low && root.val <= high){
+                return root.val + rangeSumBST(root.left , low , high) +
+                    rangeSumBST(root.right , low , high);
+            }
+            else{
+                return rangeSumBST(root.left , low , high) +
+                    rangeSumBST(root.right , low , high);
+            }  
+        } 
+        else{
+            return 0;
+        }
+    }
+}
+    
+}
