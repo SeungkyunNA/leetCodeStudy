b550e65c224601b6def0883e00d8260191477dfb
SeungkyunNA <n.jake270705@gmail.com>
2023-03-05 09:18:21 +0100

    done
    done

--- DS/ListNode.java
+++ DS/ListNode.java
@@ -6,4 +6,14 @@ public class ListNode {
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public String toString() {
+        String s = "{" + this.val + "}";
+        ListNode current = this;
+        while (current.next != null) {
+            current = current.next;
+            s = s + " - " + "{" + current.val + "}";
+        }
+        return s;
+    }
 }
